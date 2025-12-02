293a0ec35d92bedb7d565d9d11de75d703a59f17
Omar Romera <oromeraa@uoc.edu>
2025-12-03 00:21:19 +0100

    Added article component

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 oromeraa
 Omar Romera Aller
 ## Ejercicio 1
-Hecho. Pero he tenido lios con iniciar git en una directorio superior. Al final lo he iniciado en le mismo proyecto de Angular. He renombrado el README.md generado por Angular a README_angular.md y he creado uno nuevo llamado README.md que es el de la PEC de la asignatura.
+Hecho. Pero he tenido lios con iniciar git en una directorio superior. Al final lo he iniciado en le mismo proyecto de Angular. He renombrado el README.md generado por Angular a README_angular.md y he creado uno nuevo llamado README.md que es el de la PEC de la asignatura. Se ha creado usando Angular actual pero con la estructura antigua que sigue el libro de la bibliografía. Es decir, sin standalone ni routing y con style CSS.
 ## Ejercicio 2
-El libro 
+Juego de mesa creado y añadido al index.html.
 ## Ejercicio 3
 ## Ejercicio 4
 ## Ejercicio 5
---
+++ src/app/article-item/article-item.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Article } from '../model/article';
+
+@Component({
+    selector: 'app-article-item',
+    templateUrl: './article-item.component.html',
+    styleUrl: './article-item.component.css'
+})
+export class ArticleItemComponent {
+    article: Article = {
+        name: 'Mattel Games - Uno No Mercy',
+        imageUrl: 'https://images.mattel.net/image/upload/w_180,h_180,f_auto,c_pad/shop-emea-prod/products/jhypq16aafd0yqm1coyv_82ba9a3f-3be0-4b24-bd58-339571dab4b2.png',
+        price: 10.00,
+        isOnSale: true,
+        quantityInCart: 0
+    };
+
+    increaseQuantity(): void {
+        this.article.quantityInCart++;
+    }
+
+    decreaseQuantity(): void {
+        // Controlamos que no baje de 0
+        if (this.article.quantityInCart > 0) {
+            this.article.quantityInCart--;
+        }
+    }
+}
