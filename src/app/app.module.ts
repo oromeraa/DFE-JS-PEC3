b963d4869552165984c612efd8c89732a219a0cc
Omar Romera <oromeraa@uoc.edu>
2025-12-03 04:22:01 +0100

    Bootstrap navbar && clear debug code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ArticleItemComponent } from './article-item/article-item.component';
-import { DebugPanelComponent } from './debug-panel/debug-panel.component';
 import { ArticleListComponent } from './article-list/article-list.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
@@ -11,7 +10,6 @@ import { NavbarComponent } from './navbar/navbar.component';
   declarations: [
     AppComponent,
     ArticleItemComponent,
-    DebugPanelComponent,
     ArticleListComponent,
     NavbarComponent
   ],
