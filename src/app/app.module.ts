4fab0de890e7b505f62311b60d8b1d330995080b
Omar Romera <oromeraa@uoc.edu>
2025-12-03 06:57:18 +0100

    Forms without validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ArticleItemComponent } from './article-item/article-item.component';
 import { ArticleListComponent } from './article-list/article-list.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ArticleNewTemplateComponent } from './article-new-template/article-new-template.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ArticleItemComponent,
     ArticleListComponent,
-    NavbarComponent
+    NavbarComponent,
+    ArticleNewTemplateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/article-new-template/article-new-template.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Article } from '../model/article';
+
+@Component({
+  selector: 'app-article-new-template',
+  templateUrl: './article-new-template.component.html',
+  styleUrls: ['./article-new-template.component.css']
+})
+export class ArticleNewTemplateComponent {
+
+  constructor() {
+    // no debemos crear Article, solo manejar el formulario
+   }
+  createArticle(articleForm: any) {
+    if (articleForm.valid) {
+      // Se imprime por consola el objeto article recogido del formulario [cite: 229]
+      console.log('Artículo nuevo:', articleForm.value.article);
+    } else {
+      console.error('El formulario contiene errores');
+    }
+  }
+}
